4b79e18722f97fa9cfffffc91f0a93febe467186
Vicky <127982655+vh232@users.noreply.github.com>
2024-01-03 15:24:29 +0000

    delete post and get all requests

--- routes/routes.js
+++ routes/routes.js
@@ -5,28 +5,6 @@ const shoppingSitesModel = require("../models/shopping-model");
 const socialsSitesModel = require("../models/socials-model");
 const router = express.Router();
 
-router.post("/post", async (req, res) => {
-  const data = new suggestedSitesModel({
-    name: req.body.name,
-    url: req.body.url,
-  });
-
-  try {
-    const dataToSave = await data.save();
-    res.status(200).json(dataToSave);
-  } catch (error) {
-    res.status(400).json({ message: error.message });
-  }
-});
-
-router.get("/getAll", async (req, res) => {
-  try {
-    const data = await suggestedSitesModel.find();
-    res.json(data);
-  } catch (error) {
-    res.status(500).json({ message: error.message });
-  }
-});
 
 //Get by name Method
 router.get("/getOne/:category", async (req, res) => {
